410523982fc1deb0d01d7c78740cea13e90e67b9
pscs <119119194+ChyKusuma@users.noreply.github.com>
2026-05-25 17:31:20 +0700

    updated

--- worker/main.js
+++ worker/main.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
 
     if (url.hostname === "foundation.sphinxfnd.org") {
-      const foundationUrl = new URL("/foundation" + url.pathname, url);
+      const foundationUrl = new URL("/fnd" + url.pathname, url);
       return env.ASSETS.fetch(foundationUrl);
     }
 
